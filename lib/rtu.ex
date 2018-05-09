967353b1b1d556c3b225195f3acb9ab262120a22
sanyospb <sanyospb@gmail.com>
2018-05-09 17:11:51 +0300

    unwrap return error

--- lib/rtu.ex
+++ lib/rtu.ex
@@ -1,15 +1,16 @@
 defmodule Modbus.Rtu do
   @moduledoc false
+  require Logger
   alias Modbus.Helper
   alias Modbus.Request
   alias Modbus.Response
 
   def pack_req(cmd) do
-    cmd |> Request.pack |> wrap
+    cmd |> Request.pack() |> wrap
   end
 
   def parse_req(wraped) do
-    wraped |> unwrap |> Request.parse
+    wraped |> unwrap |> Request.parse()
   end
 
   def pack_res(cmd, values) do
@@ -17,31 +18,43 @@ defmodule Modbus.Rtu do
   end
 
   def parse_res(cmd, wraped) do
-    Response.parse(cmd, wraped |> unwrap)
+    res = unwrap(wraped)
+
+    if res do
+      Response.parse(cmd, res)
+    else
+      :error
+    end
   end
 
   def res_len(cmd) do
-    Response.length(cmd) + 2;
+    Response.length(cmd) + 2
   end
 
   def req_len(cmd) do
-    Request.length(cmd) + 2;
+    Request.length(cmd) + 2
   end
 
-  #CRC is little endian
-  #http://modbus.org/docs/Modbus_over_serial_line_V1_02.pdf page 13
+  # CRC is little endian
+  # http://modbus.org/docs/Modbus_over_serial_line_V1_02.pdf page 13
   def wrap(payload) do
     <<crc_hi, crc_lo>> = Helper.crc(payload)
     <<payload::binary, crc_lo, crc_hi>>
   end
 
-  #CRC is little endian
-  #http://modbus.org/docs/Modbus_over_serial_line_V1_02.pdf page 13
+  # CRC is little endian
+  # http://modbus.org/docs/Modbus_over_serial_line_V1_02.pdf page 13
   def unwrap(data) do
     size = :erlang.byte_size(data) - 2
     <<payload::binary-size(size), crc_lo, crc_hi>> = data
-    <<^crc_hi, ^crc_lo>> = Helper.crc(payload)
-    payload
-  end
 
+    case Helper.crc(payload) do
+      <<^crc_hi, ^crc_lo>> ->
+        payload
+
+      _ ->
+        Logger.error("[Modbus.Rtu] -> wrong response")
+        nil
+    end
+  end
 end
